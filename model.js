ab2c409264b57b472873a77df7065bd2eb355029
Blake Del Rey <Blake@Blakes-MacBook-Pro.local>
2016-08-01 15:57:24 -0700

    Added 7th movie to model.

--- model.js
+++ model.js
@@ -4,5 +4,6 @@ var moviesArray = '{ "movies" : [' +
 '{ "movieName":"The Social Network" , "releaseYear":"(2010)" },' +
 '{ "movieName":"Jaws" , "releaseYear":"(1975)" },' +
 '{ "movieName":"Star Wars Episode V: The Empire Strikes Back" , "releaseYear":"(1980)" },' +
+'{ "movieName":"Star Wars Episode VI: Return of the Jedi" , "releaseYear":"(1983)" },' +
 '{ "movieName":"Close Encounters of the Third Kind" , "releaseYear":"(1977)" } ]}';
 
