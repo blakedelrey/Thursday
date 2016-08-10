08eafcf4d35d013192a170a9a1e91b8a45ba36dd
Blake Del Rey <Blake@Blakes-MacBook-Pro.local>
2016-08-09 20:35:23 -0700

    Added if/else for filtering.

--- controller.js
+++ controller.js
@@ -1,7 +1,12 @@
 var movielist = JSON.parse(moviesArray) 
 var table = document.getElementById("movietable");
 console.log(movielist.movies.length);
+	var thgrossMin = 1;
+	var thgrossMax = 6;
+	var openingGrossMin = 1
+	var openingGrossMax = 50
 for(var i = 0; i < movielist.movies.length; i++) {
+if (movielist.movies[i].thGross >= thgrossMin && movielist.movies[i].thGross <= thgrossMax && movielist.movies[i].openingGross >= openingGrossMin && movielist.movies[i].openingGross<= openingGrossMax ) {
 	var row = table.insertRow(i);
 	var cell1 = row.insertCell(0);
 	var cell2 = row.insertCell(1);
@@ -12,5 +17,8 @@ for(var i = 0; i < movielist.movies.length; i++) {
 	cell2.innerHTML = movielist.movies[i].title; 
 	cell3.innerHTML = movielist.movies[i].openingDay;
 	cell4.innerHTML = movielist.movies[i].thGross;
-	cell5.innerHTML = movielist.movies[i].openingGross;	
+	cell5.innerHTML = movielist.movies[i].openingGross;
+} else {
+	var row = table.insertRow(i);
+}	
 } 
